c7f254f6ccf770c8708cdf0d8b793ad0303618d5
melissamuthoni <melissamk886@gmail.com>
2026-04-28 11:58:16 +0300

    commit

--- script.js
+++ script.js
@@ -6,12 +6,13 @@ document.getElementById("akanform").addEventListener("submit", function(e){
  let year = parseInt(document.getElementById("year").value)
  let gender = parseInt(document.getElementById("gender").value)
 
-//  validation
+// validation
 If (day < 1 || day > 31 || month < 1 || month > 12 || gender === "") {
 alert("please enter a valid date and select gender.");
 return;
 }
 
+// 
 
 
 
