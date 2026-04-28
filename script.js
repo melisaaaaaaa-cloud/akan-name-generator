6f5387eba73d1460d08c44d446940630916b980b
melissamuthoni <melissamk886@gmail.com>
2026-04-28 11:41:50 +0300

    save changes

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-document.getElementById("akanform").addEventListener("submit", fucntion(e
+document.getElementById("akanform").addEventListener("submit", function(e){
+ e.preventDefault();
+ 
+ let day = parseInt(document.getElementById("day").value)
+ let month = parseInt(document.getElementById("month").value)
+ let year = parseInt(document.getElementById("year").value)
+ let gender = parseInt(document.getElementById("gender").value)
+
+//  validation
+If (day < 1 || day > 31 || month < 1 || month > 12 || gender === "") {
+
+}
+}
