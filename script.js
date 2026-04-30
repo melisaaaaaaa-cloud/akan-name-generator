01ea84dcfc7f9a044d1541c0251450acc437f4f2
melissamuthoni <melissamk886@gmail.com>
2026-04-30 18:24:50 +0300

    validation and array entry

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("akanform").addEventListener("submit", function(e) {e.preventDefault();
+let day = parseInt(document.getElementById("day").value);
+let month = parseInt(document.getElementById("month").value);
+let year = parseInt(document.getElementById("year").value);
+let gender = parseInt(document.getElementById("gender").value);
+
+// validation
+if (day < 1|| day > 31 || month < 1 || month > 12 || gender === "" ) { 
+alert("please enter a valid date and select gender.");
+return;
+}
+// arrays
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+let maleNames = ["Kwasi",  ]
+
+
+
+}
