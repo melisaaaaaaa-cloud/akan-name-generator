3b13e517429f149483a5197a26a8c4ec2618b438
melissamuthoni <melissamk886@gmail.com>
2026-04-30 19:47:15 +0300

    js formula entries

--- script.js
+++ script.js
@@ -1,37 +1,51 @@
-console.log("JS is working");
+
+document.addEventListener("DOMContentLoaded", function () {
+    Document.getElementById("akanForm").addEventListener("submit", function(e) {
+        e.preventDefault();
+        let day = parseInt(document.getElementById("day").value);
+        let month = parseInt(document.getElementById("month").value);
+        let year = parseInt(document.getElementById("year").value);
+        let gender = parseInt(document.getElementById("gender").value);
+
+        if(day < 1 || day > 31 || month < 1 || month > 12 || gender === "") {
+         alert("please enter a valid date and select gender.");
+         return;
+        }
+        let date = new Date(year, month - 1, day);
+        let dayOfWeek = date.getDay();
+
+
+        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", ]
+
+        )
+
+    }
+ )
 
 
 
 
 
-document.getElementById("akanform").addEventListener("submit", function(e) {e.preventDefault();
-let day = parseInt(document.getElementById("day").value);
-let month = parseInt(document.getElementById("month").value);
-let year = parseInt(document.getElementById("year").value);
-let gender = parseInt(document.getElementById("gender").value);
+    let date = new Date(year, month - 1, day);
+    let dayOfWeek = date.getDay();
 
-// Validation
-if (day < 1|| day > 31 || month < 1 || month > 12 || gender === "" ) { 
-alert("please enter a valid date and select gender.");
-return;
-}
-// arrays
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
-let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    let akanName;
 
-if (gender === "male") {
-    akanName = maleNames[dayOfWeek];
-} else{
-    akanName = femaleNames[dayOfWeek];
-}
-// Display result
-document.getElementById("result").innerText =
-'You were born on a ${days[dayOfWeek]}. Your Akan name is ${akanName}.';
+    if (gender === "male") {
+      akanName = maleNames[dayOfWeek];
+    } else {
+      akanName = femaleNames[dayOfWeek];
+    }
 
-// Reset form
-document.getElementById("akanForm").reset();
+    document.getElementById("result").innerText =
+      `You were born on a ${days[dayOfWeek]}. Your Akan name is ${akanName}.`;
 
+    document.getElementById("akanForm").reset();
+  });
 
 });
